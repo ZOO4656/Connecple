b7d183016af8883850fb96a8e961e2c928e7688e
ZOO4656 <zoo4656keepkeep@gmail.com>
2019-03-24 10:36:45 +0900

    ファイル名修正

--- main.rb
+++ main.rb
@@ -6,11 +6,11 @@ require 'digest/sha1'
 
 
 get '/' do
-  	client = Mysql2::Client.new(host: "0.0.0.0", username: "root", password: 'root', database: 'connecple')
-  	results = client.query("SELECT * FROM comment")
-  	@ary = Array.new
-  	results.each {|row| @ary << row}
-  	erb :comment
+	client = Mysql2::Client.new(host: "0.0.0.0", username: "root", password: 'root', database: 'connecple')
+	results = client.query("SELECT * FROM comment")
+	@ary = Array.new
+	results.each {|row| @ary << row}
+	erb :comment
 end
 
 #メインメニュー
@@ -24,32 +24,36 @@ post '/main' do
 end
 
 #新規登録画面の取得
-get '/user_newSignup' do
-  erb :newSign_up
+get '/user_signup' do
+  erb :sign_up
 end
 
 #新規登録画面
-post '/user_newSignup' do
+post '/user_signup' do
   client = Mysql2::Client.new(host: "0.0.0.0", username: "root", password: 'root', database: 'connecple')
 
   #パスワードのハッシュ化に伴いソルト生成
   userSolt = SecureRandom.alphanumeric(16)
   convertHashPass = Digest::SHA1.hexdigest("#{params['password']},#{userSolt}")
   client.query("INSERT INTO user (name,password,user_id,pass_solt) VALUES ('#{params['name']}','#{convertHashPass}','#{params['user_id']}','#{userSolt}')")
-  erb :newSign_up
-end
-
-#サインアップ時のerbファイルを取得
-get '/user_signup' do
   erb :sign_up
 end
 
-#サインアップの整合性確認
-post '/user_signup' do
+#サインアップ時のerbファイルを取得
+get '/user_signin' do
   client = Mysql2::Client.new(host: "0.0.0.0", username: "root", password: 'root', database: 'connecple')
-  @results = client.query("SELECT name,password,pass_solt FROM user")
-  results.each do |dbUserData|
-  puts dbUserData
-  end
+  results = client.query("SELECT * FROM user")
+  @ary = Array.new
+  results.each {|row| @ary << row}
+  erb :sign_in
+end
+
+#サインアップの整合性確認
+post '/user_signin' do
+  client = Mysql2::Client.new(host: "0.0.0.0", username: "root", password: 'root', database: 'connecple')
+  results = client.query("SELECT * FROM user")
+  @ary = Array.new
+  results.each {|row| @ary << row}
+  erb :sign_in
 end
 
